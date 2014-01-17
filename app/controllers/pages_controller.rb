78c8c4183d34504f72ed5bad644671b09c7a9839
robwhelan <whelan@gmail.com>
2014-01-16 22:03:26 -0500

    removed google map and cookie stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,6 +19,16 @@ class PagesController < ApplicationController
   end
   
   def pcs_move_to_charleston
+    session[:content] = params[:content]
+    
+    if session[:content] == "a"
+      	@title = "PCS Move to Charleston with Real Time Tips"
+      	@headline = "So you're PCSing to Charleston and you need some tips!"
+      	@section1 = "our real-time tips are from regular Charleston residents who want to help military families moving to Charleston. It's current and practical, perfect for a military family planning an exciting (but stressful) move."
+      	@section2 = "the Womditty community shares photos of stuff you actually want to see, like where the clean playgrounds are!"
+      	@section3 = "we're just getting started, so we need your questions to unlock the knowledge. What tips are you looking for? Best schools near the base? Where do I live to get away from work but not have too far of a drive? Which places should I avoid? Log in and ask."
+      	@section4 = "Over 50 local residents have signed on to help military families. Don't learn it the hard way-- ask your question now and you'll be much happier."
+    end
   end
       
 end
